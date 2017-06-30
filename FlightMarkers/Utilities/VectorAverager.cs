b1a70c1cb2cd2089822540bbd117f4574a6e291c
DefiantZombie <adobrinin@gmail.com>
2017-06-29 23:59:55 -0700

    Added vector averaging utilities.
    * Adds a tiny overhead for guaranteed results.

---
+++ FlightMarkers/Utilities/VectorAverager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+
+namespace FlightMarkers.Utilities
+{
+    public class VectorAverager
+    {
+        private Vector3 _sum = Vector3.zero;
+        private uint _count;
+
+
+        public void Add(Vector3 v)
+        {
+            _sum += v;
+            _count++;
+        }
+
+
+        public Vector3 Get()
+        {
+            if (_count > 0)
+                return _sum / _count;
+
+            return Vector3.zero;
+        }
+
+
+        public void Reset()
+        {
+            _sum = Vector3.zero;
+            _count = 0;
+        }
+    }
+}
---
+++ FlightMarkers/Utilities/WeightedVectorAverager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+
+namespace FlightMarkers.Utilities
+{
+    public class WeightedVectorAverager
+    {
+        private Vector3 _sum = Vector3.zero;
+        private float _totalWeight;
+
+
+        public void Add(Vector3 v, float weight)
+        {
+            _sum += v * weight;
+            _totalWeight += weight;
+        }
+
+
+        public Vector3 Get()
+        {
+            if (_totalWeight > 0f)
+                return _sum / _totalWeight;
+
+            return Vector3.zero;
+        }
+
+
+        public float GetTotalWeight()
+        {
+            return _totalWeight;
+        }
+
+
+        public void Reset()
+        {
+            _sum = Vector3.zero;
+            _totalWeight = 0f;
+        }
+    }
+}
